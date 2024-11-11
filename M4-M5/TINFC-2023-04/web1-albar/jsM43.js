43e024850991d61541833476771a61c4e8bd6811
Albar19 <20230810161@uniku.ac.id>
2024-11-11 14:32:24 +0700

    neww

---
+++ M4-M5/TINFC-2023-04/web1-albar/jsM43.js
@@ -0,0 +1,12 @@
+function pesan(){
+    alert("Ini Latihan JavaSript Pertama Saya");
+}
+
+function pesanConsole(){
+    console.log("It's Work");
+}
+
+function pesanDok(){
+    document.write("Sukses <br>");
+    document.write("<a href='M4-2.html'>Kembali</a>");
+}
